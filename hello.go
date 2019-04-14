70255f3a06fab7daa4551b666c4105a481b2d9a2
Sujeet Pandey <sujeetp@dunnhumby.com>
2019-04-14 12:39:48 +0530

    Added hello.go sample go cuncurrency

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+    // create new channel of type int
+    ch := make(chan int)
+
+    // start new anonymous goroutine
+    go func() {
+        // send 42 to channel
+        ch <- 42
+    }()
+    // read from channel
+    fmt.Printf("You receive %v", <-ch)
+}
